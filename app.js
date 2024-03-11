4f8c966054db22f6afb595d924c1904bf2b1dea4
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2024-03-11 09:34:25 +0100

    fix: cors middleware

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const express = require('express');
 // create an express app
 const app = express();
 
+// cors middleware
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
 // simple middleware
 app.use((req, res) => {
     res.json({ message: 'Hello World!' });
